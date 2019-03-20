bf7deb2483b4f63aacda329ad4f12b4ec63d26e0
andres <andommartinez@gmail.com>
2019-03-20 20:19:00 +0100

    Update Lamina_equipos

--- Laminas/Lamina_Equipos.java
+++ Laminas/Lamina_Equipos.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -19,11 +20,16 @@ import javax.swing.border.MatteBorder;
 
 //https://www.geeksforgeeks.org/java-swing-jtextarea/
 //http://chuwiki.chuidiang.org/index.php?title=Ejemplos_con_JTextField
-	
+//https://docs.oracle.com/javase/tutorial/uiswing/components/icon.html
+//http://www.sc.ehu.es/sbweb/fisica/cursoJava/applets/diseno/gridbag.htm
+
+
 public class Lamina_Equipos extends JPanel implements ActionListener{
 	
     protected JTextField textField;
+    protected JTextField textField2;
     protected JTextArea textArea;
+ 
     private final static String newline = "\n";
     
     JButton boton1 = new JButton ("Juego");
@@ -37,14 +43,21 @@ public class Lamina_Equipos extends JPanel implements ActionListener{
         img = Toolkit.getDefaultToolkit().createImage("src/misiles_v4/space_fondo.jpg");
 		
       //Text Field
-		textField = new JTextField(10);
+		textField = new JTextField(15);
 		textField.addActionListener(this);
+		
+		textField2 = new JTextField(15);
+		textField2.addActionListener(this);
 		
 	    textArea = new JTextArea(10,20);
 	    textArea.setEditable(false); //no deja editar el texto
 	      
 	    JScrollPane scrollPane = new JScrollPane(textArea); //se añade un scroll panel al textArea
         
+	    //Label
+	   JLabel jLabel_texto1 = new JLabel ("Nombre equipo");
+	   JLabel jLabel_texto2 = new JLabel ("Tipo equipo");
+
         
         //Add Components to this panel.
 		setLayout(new GridBagLayout());
@@ -62,39 +75,76 @@ public class Lamina_Equipos extends JPanel implements ActionListener{
 		boton1.setContentAreaFilled(false);
 		boton1.setBorderPainted(false);
 		boton1.setForeground(Color.RED);
-		c.fill = GridBagConstraints.HORIZONTAL;
-			
-		c.gridx = 0;
-		c.gridy = 0;
-		add(boton1,c);
+		
+		
 		
 		//Botón 2
 		boton2.setBackground(Color.lightGray);
 		boton2.setBorder(new MatteBorder(2,2,2,2, Color.WHITE)); //matteborder define el grosor de cada extremo del boton y el color
 		
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 3;
-		add(boton2,c);
+
 		
+	    //add label
+        jLabel_texto1.setForeground(Color.red);
+		c.gridx = 0;
+		c.gridy = 1;
+	    add(jLabel_texto1, c);
+	    
+	    //add label
+        jLabel_texto2.setForeground(Color.red);
+		c.gridx = 0;
+		c.gridy = 2;
+	    add(jLabel_texto2, c);
 		
         //Text Field
         
         c.fill = GridBagConstraints.HORIZONTAL;
         c.weightx = 0;
         c.weighty = 10;
-		c.gridx = 0;
+		c.gridx = 1;
 		c.gridy = 1;
         add(textField, c);
         
+        //Text field 2
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.weighty = 10;
+		c.gridx = 1;
+		c.gridy = 2;
+        add(textField2, c);
+        
+        //boton 1
+        
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 4;
+		add(boton1,c);
+        
+        
+        //boton 2
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 4;
+		add(boton2,c);
+
+
+        
         //Scroll Panel
         
+        c.insets = new Insets(20,0,20,0);
+        
         c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = GridBagConstraints.REMAINDER;
         c.weightx = 0;
         c.weighty = 0;
 		c.gridx = 0;
-		c.gridy = 2;
+		c.gridy = 3;
         add(scrollPane, c);
+        
+
+        
+
+
         
        
 	}
